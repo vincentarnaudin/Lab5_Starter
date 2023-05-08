56cd040df169c8119090f31a580f1b9a1b1f9efe
Vincent <arnaudinvi@cy-tech.fr>
2023-05-07 21:31:34 -0700

    explore.js finished

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -18,11 +18,9 @@ function init() {
     for (let i = 0; i < voices.length; i++) {
       const option = document.createElement("option");
       option.textContent = `${voices[i].name} (${voices[i].lang})`;
-
       if (voices[i].default) {
         option.textContent += " — DEFAULT";
       }
-
       option.setAttribute("data-lang", voices[i].lang);
       option.setAttribute("data-name", voices[i].name);
       voiceSelect.appendChild(option);
